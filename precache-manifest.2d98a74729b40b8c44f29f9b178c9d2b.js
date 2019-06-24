d6b9209fc20bf6b789e893c8b29c3b3a4584cabb
Wuilliam Lacruz <wuilliam321@gmail.com>
2019-06-23 21:20:13 -0300

    Updates

---
+++ precache-manifest.2d98a74729b40b8c44f29f9b178c9d2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38854d519bd8c74972e4a9605c5fc434",
+    "url": "/clicker-game/index.html"
+  },
+  {
+    "revision": "9e1e8089fd509d069048",
+    "url": "/clicker-game/static/css/main.435c2168.chunk.css"
+  },
+  {
+    "revision": "6bea2a1750d5e71fa6f7",
+    "url": "/clicker-game/static/js/2.91120321.chunk.js"
+  },
+  {
+    "revision": "9e1e8089fd509d069048",
+    "url": "/clicker-game/static/js/main.9851e364.chunk.js"
+  },
+  {
+    "revision": "cd401eb4e9e99ab75dd2",
+    "url": "/clicker-game/static/js/runtime~main.8efb6c9b.js"
+  }
+]);
